3656bd3d0770078a4a3ca39de171619802abffc4
Robbie Perrott <robbieperrott@gmail.com>
2025-07-20 11:34:14 +0200

    Fix avatar colors

--- app/components/userToggle/userAvatar.tsx
+++ app/components/userToggle/userAvatar.tsx
@@ -11,14 +11,14 @@ export default function UserAvatar({
     const currentUser = use(user);
     const userNames = currentUser.name.split(' ');
     const initials = `${userNames[0].charAt(0)}${userNames[1].charAt(0)}`
-    return <Avatar className={`${userColorCss(currentUser.id)} flex items-center place-content-center`}>
-            <AvatarFallback className="text-sm">{initials}</AvatarFallback> 
+    return <Avatar className={"flex items-center place-content-center"}>
+            <AvatarFallback className={`${userColorCss(currentUser.id)} text-sm`}>{initials}</AvatarFallback> 
         </Avatar>
 }
 
-const colors = ["bg-red-200", "bg-orange-200", "bg-yellow-200", "bg-lime-200", "bg-green-200", "bg-teal-200", "bg-blue-200",  "bg-purple-200", "bg-pink-200", "bg-rose-200"];
+const colors = ["bg-red-200", "bg-orange-200", "bg-yellow-200", "bg-lime-200", "bg-green-200", "bg-teal-200", "bg-blue-200",  "bg-purple-200", "bg-pink-200", "bg-slate-200"];
 
 const userColorCss = (userId: number) => {
-  const index = userId % colors.length;
+  const index = (userId - 1) % colors.length;
   return colors[index];
 }
